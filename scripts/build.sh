20d923fdfe8960869c5bd4a96fb1149fb683aa5c
Karim Alibhai <karim@alibhai.co>
2019-07-11 15:58:16 -0400

    Fix: md5 hashing for linux (again)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,14 @@ set -o pipefail
 
 tmp="`mktemp -d`"
 
+function create_hash() {
+	if which md5 &>/dev/null; then
+		cat "$1" | md5
+	else
+		cat "$1" | md5sum | cut -d- -f1
+	fi
+}
+
 # compile an initial copy with just babel, since
 # that should always work
 rm -rf dist
@@ -13,7 +21,7 @@ babel src -d dist
 echo ""
 echo "building cli using babel-compiled cli:"
 node dist/cli.js build src/cli.js
-echo "cli.dist.js - `cat cli.dist.js | md5`"
+echo "cli.dist.js - `create_hash cli.dist.js`"
 
 # compile several times with the tool itself to ensure
 # that the build works
@@ -22,13 +30,7 @@ for ((i=0;i<5;i++)); do
 	echo "build #$[i+1]:"
 	./cli.dist.js build src/cli.js
 
-	hash=""
-	if which md5 &>/dev/null; then
-		hash="`cat cli.dist.js | md5`"
-	else
-		hash="`cat cli.dist.js | md5sum | cut -d- -f1`"
-	fi
-
+	hash="`create_hash cli.dist.js`"
 	echo "cli.dist.js - $hash"
 	cp "cli.dist.js" "${tmp}/cli.${hash}.js"
 done
