e7ae95c8bd03e3ce36cd3b7e722fa38453f04faf
Karim Alibhai <karim@alibhai.co>
2019-07-15 06:02:46 -0400

    Fix: jest locator

--- src/commands/test.js
+++ src/commands/test.js
@@ -4,27 +4,49 @@
  */
 
 import * as fs from 'fs'
+import * as path from 'path'
 import { spawnSync } from 'child_process'
 
 import * as dotenv from 'dotenv'
 
 import { isCI } from '../config'
+import * as performance from '../perf'
+
+function findJest() {
+	const jestDirectory = require.resolve('jest').split('/')
+	while (jestDirectory.length > 0) {
+		if (jestDirectory[jestDirectory.length - 1] === 'node_modules') {
+			break
+		}
+		jestDirectory.pop()
+	}
+	if (!jestDirectory.length) {
+		throw new Error(`Could not find jest`)
+	}
+
+	const jestPath = jestDirectory.join('/') + '/jest'
+	const jestPackage = require(`${jestPath}/package.json`)
+	return path.resolve(jestPath, jestPackage.bin.jest)
+}
 
 export async function testCommand(argv) {
 	const testFlags = argv.input.slice(1)
-	const env = JSON.parse(JSON.stringify(process.env))
 
-	try {
-		const envContent = fs.readFileSync('.env-test', 'utf8')
-		Object.assign(env, dotenv.parse(envContent))
-	} catch (error) {
-		if (error.code !== 'ENOENT') {
-			throw error
+	const env = await performance.measure('load env', () => {
+		const env = JSON.parse(JSON.stringify(process.env))
+		try {
+			const envContent = fs.readFileSync('.env-test', 'utf8')
+			return Object.assign(env, dotenv.parse(envContent))
+		} catch (error) {
+			if (error.code !== 'ENOENT') {
+				throw error
+			}
 		}
-	}
+		return env
+	})
 
 	const jestArgs = [
-		require.resolve('jest'),
+		await performance.measure('find jest', findJest),
 		'--coverage',
 		'--env=node',
 		'--testPathPattern="src\\/.*\\/__tests__\\/test-.*\\.js"',
