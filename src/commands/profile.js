61e6adea6d549cf0f1f232437d1102577aeef4c2
Karim Alibhai <karim@alibhai.co>
2019-07-21 00:05:57 -0400

    Add: profiling promises

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -10,12 +10,17 @@ import babel from '@babel/core'
 import * as t from '@babel/types'
 import template from '@babel/template'
 import { v4 as uuid } from 'uuid'
+import createDebug from 'debug'
 
 import { cliReporter } from '../profiler/reporters/cli'
 
+const debug = createDebug('wiz')
 const globalID = '__PROFILER__'
 const startRecording = template(`global.GLOBALID.markStart(ID)`)
 const stopRecording = template(`global.GLOBALID.markEnd(ID, LINE, FILE)`)
+const recordPromise = template(`global.GLOBALID.measurePromise(ID, LINE, FILE, () => {
+	return ARG
+})`)
 
 export const profileFlags = {
 	reporter: {
@@ -29,6 +34,36 @@ export const profileFlags = {
 		alias: 't',
 		default: 10,
 	},
+
+	noExternal: {
+		type: 'boolean',
+		alias: 'i',
+		default: false,
+	},
+}
+
+function getModuleName(file) {
+	file = file.split('/')
+
+	for (let i = file.length - 1; i > -1; --i) {
+		if (file[i] === 'node_modules') {
+			if (file[i + 1][0] === '@') {
+				return {
+					moduleName: file[i + 1] + '/' + file[i + 2],
+					file: file.slice(i + 1).join('/'),
+				}
+			}
+			return {
+				moduleName: file[i + 1],
+				file: file.slice(i + 1).join('/'),
+			}
+		}
+	}
+
+	return {
+		moduleName: '(local)',
+		file: file.join('/').replace(process.cwd(), '.'),
+	}
 }
 
 export function profileCommand(argv) {
@@ -39,6 +74,7 @@ export function profileCommand(argv) {
 
 	const entrypoint = argv.input[1]
 	const reportEvents = (function(reporterPath) {
+		debug(`Loading reporter: ${reporterPath}`)
 		switch (reporterPath) {
 			case 'cli':
 				return cliReporter
@@ -61,108 +97,133 @@ export function profileCommand(argv) {
 				}
 
 				const ID = t.stringLiteral(uuid())
-				path.replaceWithMultiple([
-					startRecording({
-						ID,
-						GLOBALID: globalID,
-					}),
-					path.node,
-					stopRecording({
-						ID,
-						GLOBALID: globalID,
-						LINE: t.numericLiteral(path.node.loc.start.line),
-						FILE: t.stringLiteral(filename),
-					}),
-				])
+				path.replaceWith(
+					t.sequenceExpression([
+						startRecording({
+							ID,
+							GLOBALID: globalID,
+						}).expression,
+						path.node.expression,
+						stopRecording({
+							ID,
+							GLOBALID: globalID,
+							LINE: t.numericLiteral(path.node.loc.start.line),
+							FILE: t.stringLiteral(filename),
+						}).expression,
+					]),
+				)
+				path.skip()
+			},
+			AwaitExpression(path) {
+				if (!path.node.loc) {
+					return
+				}
+
+				const ID = t.stringLiteral(uuid())
+				path.node.argument = recordPromise({
+					ID,
+					GLOBALID: globalID,
+					LINE: t.numericLiteral(path.node.loc.start.line),
+					FILE: t.stringLiteral(filename),
+					ARG: path.node.argument,
+				}).expression
+				path.skip()
 			},
 		},
 	})
 
-	let compiling = false
-	pirates.addHook(
-		function(code, filename) {
-			if (filename.includes('babel') || compiling) {
-				return code
-			}
-
-			try {
-				compiling = true
-				return babel.transformSync(code, {
-					plugins: [transform(filename)],
-				}).code
-			} finally {
-				compiling = false
-			}
-		},
-		{
-			ignoreNodeModules: false,
-		},
-	)
-
-	const Profiler = (global[globalID] = {
-		data: new Map(),
-		timers: new Map(),
-
-		markStart(id) {
-			this.timers.set(id, Date.now())
-		},
-
-		markEnd(id, line, file) {
-			const start = this.timers.get(id)
-
-			let moduleName = '(local)'
-			const scopedModName = file.match(/\/node_modules\/(@.*?\/.*?)(\/.*$)/)
-			if (scopedModName) {
-				moduleName = scopedModName[1]
-				file = moduleName + scopedModName[2]
-			} else {
-				const modName = file.match(/\/node_modules\/(.*?)(\/.*$)/)
-				if (modName) {
-					moduleName = modName[1]
-					file = moduleName + modName[2]
-				} else {
-					file = file.replace(process.cwd(), '.')
+	return new Promise(resolveExit => {
+		let compiling = false
+		pirates.addHook(
+			function(code, filename) {
+				if (filename.includes('babel') || compiling) {
+					return code
 				}
-			}
 
-			const fileID = file + ':' + line
-			const prev = this.data.get(fileID)
-			if (prev) {
-				prev.duration += Date.now() - start
-				prev.ticks++
-			} else {
-				this.data.set(fileID, {
-					duration: Date.now() - start,
-					line,
-					moduleName,
-					file,
-					fileID,
-					ticks: 1,
-				})
-			}
-		},
+				try {
+					compiling = true
+					debug(`Instrumenting: ${filename}`)
+					const transformedCode = babel.transformSync(code, {
+						plugins: [transform(filename)],
+					}).code
+					debug(`${filename} => ${transformedCode}`)
+					return transformedCode
+				} finally {
+					compiling = false
+				}
+			},
+			{
+				ignoreNodeModules: argv.flags.noExternal,
+			},
+		)
 
-		exit() {
-			reportEvents(
-				Array.from(Profiler.data.values())
-					.sort((a, b) => {
-						return b.duration - a.duration
+		const Profiler = (global[globalID] = {
+			data: new Map(),
+			timers: new Map(),
+
+			markStart(id) {
+				debug(`Event started: ${id}`)
+				this.timers.set(id, Date.now())
+			},
+
+			markEnd(id, line, filepath) {
+				debug(`Event ended: ${id}`)
+				const start = this.timers.get(id)
+
+				const { moduleName, file } = getModuleName(filepath)
+				const fileID = file + ':' + line
+
+				const prev = this.data.get(fileID)
+				if (prev) {
+					prev.duration += Date.now() - start
+					prev.ticks++
+				} else {
+					this.data.set(fileID, {
+						duration: Date.now() - start,
+						line,
+						moduleName,
+						file,
+						fileID,
+						ticks: 1,
 					})
-					.filter(event => {
-						return event.duration >= argv.flags.minThreshold
-					}),
-			)
-		},
+				}
+			},
+
+			async measurePromise(id, line, file, fn) {
+				try {
+					Profiler.markStart(id)
+					return await fn()
+				} catch (error) {
+					throw error
+				} finally {
+					Profiler.markEnd(id, line, file)
+				}
+			},
+
+			exit() {
+				reportEvents(
+					Array.from(Profiler.data.values())
+						.sort((a, b) => {
+							return b.duration - a.duration
+						})
+						.filter(event => {
+							return event.duration >= argv.flags.minThreshold
+						}),
+				)
+				resolveExit()
+			},
+		})
+
+		process.on('beforeExit', () => Profiler.exit())
+
+		const exit = process.exit
+		process.exit = code => {
+			Profiler.exit()
+			return exit(code)
+		}
+
+		// avoid dropping require
+		debug(`Loading: ${entrypoint}`)
+		require(path.resolve(process.cwd(), entrypoint))
 	})
-
-	process.on('beforeExit', () => Profiler.exit())
-
-	const exit = process.exit
-	process.exit = code => {
-		Profiler.exit()
-		return exit(code)
-	}
-
-	// avoid dropping require
-	const _ = require(path.resolve(process.cwd(), entrypoint))
 }
