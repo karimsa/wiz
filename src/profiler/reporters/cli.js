bb0463ff1f340a401d3c64f0b880755df0383f07
Karim Alibhai <karim@alibhai.co>
2019-08-05 02:25:28 -0400

    Add: impact filtering by threshold for profiler

--- src/profiler/reporters/cli.js
+++ src/profiler/reporters/cli.js
@@ -5,23 +5,23 @@
 
 import Table from 'cli-table'
 
-export function cliReporter(events) {
+export function cliReporter(events, { minThreshold }) {
 	const table = new Table({
 		head: ['type', 'file', 'duration', '# calls', 'impact'],
 	})
 
 	events.forEach(event => {
-		event.impact = event.duration / event.ticks
-	})
+		const impact = event.duration / event.ticks
 
-	events.forEach(event => {
-		table.push([
-			event.type,
-			event.fileID,
-			event.duration,
-			event.ticks,
-			event.impact,
-		])
+		if (impact > minThreshold) {
+			table.push([
+				event.type,
+				event.fileID,
+				event.duration,
+				event.ticks,
+				impact,
+			])
+		}
 	})
 
 	console.log(table.toString())
