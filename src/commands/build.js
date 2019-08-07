01c8b712f5705934fdcd4155b9bca2c2edfce044
Karim Alibhai <karim@alibhai.co>
2019-08-07 00:58:16 -0400

    Add: build docs

--- src/commands/build.js
+++ src/commands/build.js
@@ -13,17 +13,34 @@ import * as perf from '../perf'
 
 const nodeVersion = '8'
 
-export const buildFlags = {
-	output: {
-		type: 'string',
-		alias: 'o',
-	},
-	env: {
-		type: 'string',
-		alias: 'e',
-	},
-}
-
+/**
+ * **Usage**
+ *
+ * To build an entrypoint with wiz, you simply need to do `wiz build [path to entrypoint]`.
+ * This will take the input file (which **must** be located top-level in `src/`) and build
+ * it into the current directory, with `.dist.js` file extension instead of `.js`. wiz ensures
+ * that `*.dist.js` exists in your `.gitignore` so all output files are ignored.
+ *
+ * Since rollup does not currently having persistence caching builtin, there is no cache written
+ * out by wiz either. However, wiz may wrap rollup with a custom cache in the future which would
+ * exist in `.wiz`. As with linting, to reset the cache, you can simply run `rm -rf .wiz` - this
+ * operation is always safe.
+ *
+ * **Internals**
+ *
+ * The build tool is wrapped around wiz, and the build pipeline is as follows:
+ *
+ *  - `rollup-plugin-commonjs`: to support commonjs sources.
+ *  - `rollup-plugin-json`: to import JSON files with regular imports.
+ *  - `rollup-plugin-replace`: to replace `process.env.NODE_ENV` with production, and
+ *  any environment overrides given to wiz.
+ *  - `rollup-plugin-babel`: to transform your JS syntax into syntax that is supported
+ *  by your current node version.
+ *  - `terser`: to drop dead code, constant fold, and optimize pure functions. Mangling
+ *  and obfuscation are skipped.
+ *  - `add-shebang`: adds a shebang for node to the start of the output file.
+ *  - `chmod`: adds execution privileges to output file.
+ */
 export async function buildCommand(argv) {
 	if (argv._.length !== 2) {
 		throw new Error(`Build must specify exactly one entrypoint to compile`)
@@ -149,4 +166,60 @@ export async function buildCommand(argv) {
 		}
 		perf.observeEntries(perfEntries)
 	}
+}
+
+/**
+ * **Changing build destination**
+ *
+ * The `--output` flag allows you to customize the filename of the build output.
+ * By default, is it `[basename].dist.js` where `[basename]` is the basename of the
+ * input source file. This might be desirable since when the file is located within
+ * your source files, its parent directory names help add meaning to the file's
+ * purpose. However, as an output file, it has no parent directory so it might need
+ * a more detailed filename.
+ *
+ * ```shell
+ * $ wiz build src/foo.js
+ * # Builds into foo.dist.js
+ * $ wiz build src/foo.js -o bar.dist.js
+ * # Builds into bar.dist.js
+ * ```
+ *
+ * **Passing custom environment variables**
+ *
+ * The `--env` flag can be used to override environment variables that are known
+ * at compile-time. For instance, `NODE_ENV` is always overriden by default within
+ * `wiz` to allow projects to differentiate between their build environment vs. their
+ * execution environment.
+ *
+ * Aside from `NODE_ENV`, you can make custom overrides too. Here's an example:
+ *
+ * ```javascript
+ * // src/foo.js
+ * console.log('%s, world', process.env.MESSAGE || 'Hello')
+ * ```
+ *
+ * Building this with `wiz build src/foo.js` would output:
+ *
+ * ```javascript
+ * // foo.dist.js
+ * console.log('%s, world', process.env.MESSAGE || 'Hello')
+ * ```
+ *
+ * Building this with `wiz build src/foo.js -e MESSAGE=Bye` would output:
+ *
+ * ```javascript
+ * // foo.dist.js
+ * console.log('%s, world', 'Bye')
+ * ```
+ */
+export const buildFlags = {
+	output: {
+		type: 'string',
+		alias: 'o',
+	},
+	env: {
+		type: 'string',
+		alias: 'e',
+	},
 }
