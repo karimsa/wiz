c4c4ad5ee5fd04a3bd754cccb44d9b1478d858a2
Karim Alibhai <karim@alibhai.co>
2019-08-07 02:41:49 -0400

    Update: bench command docs

--- src/commands/bench.js
+++ src/commands/bench.js
@@ -1,3 +1,13 @@
+import * as os from 'os'
+import * as path from 'path'
+import { spawn } from 'child_process'
+
+import createDebug from 'debug'
+
+import { findSourceFiles } from '../glob'
+
+const debug = createDebug('wiz')
+
 /**
  * Writing benchmarks with `wiz` involves understanding a bit about how benchmarks are run. The benchmark
  * runner consists of two parts - a utility that is imported from `@karimsa/wiz/bench` and the benchmark CLI
@@ -72,16 +82,87 @@
  * for `go`. As such, I recommend reading Dave Cheney's blog post on
  * [How to write benchmarks in Go](https://dave.cheney.net/2013/06/30/how-to-write-benchmarks-in-go).
  */
+export async function benchCommand(argv) {
+	const runProfiler = Boolean(argv.profile)
+	const runSerially = Boolean(argv.serial || runProfiler)
 
-import * as os from 'os'
-import * as path from 'path'
-import { spawn } from 'child_process'
+	process.env.WIZ_BENCH = JSON.stringify({
+		growthFn: argv.growth,
+		maxRunTime: argv.benchTime,
+		maxIterations: argv.benchRuns,
+	})
 
-import createDebug from 'debug'
+	let targetShard = 0
+	let numBenchFiles = 0
+	const numCPUs = os.cpus().length
+	const fileShards = runSerially ? [[]] : [...new Array(numCPUs)].map(() => [])
 
-import { findSourceFiles } from '../glob'
+	for await (const { file, type } of findSourceFiles({
+		directory: path.join(process.cwd(), 'src'),
+		cache: {},
+	})) {
+		if (type === 'benchmark') {
+			++numBenchFiles
+			fileShards[targetShard].push(file)
 
-const debug = createDebug('wiz')
+			if (++targetShard === fileShards.length) {
+				targetShard = 0
+			}
+		}
+	}
+	debug(`Sharded ${numBenchFiles} benchmark files across ${numCPUs} processes`)
+
+	const goals = []
+	fileShards.forEach(shard => {
+		if (shard.length) {
+			goals.push(
+				new Promise((resolve, reject) => {
+					const args = ['-e', '"void 0"']
+
+					shard.forEach(file => {
+						args.unshift(file)
+						args.unshift('--require')
+					})
+
+					if (runProfiler) {
+						args.unshift(require.resolve('./register-profiler.dist.js'))
+						args.unshift('--require')
+					}
+
+					debug(`Spawning benchmark process: %O`, {
+						args,
+					})
+					const child = spawn(process.execPath, args, {
+						stdio: 'inherit',
+					})
+
+					child.on('close', code => {
+						if (code === 0) {
+							resolve()
+						} else {
+							reject(
+								Object.assign(new Error(), {
+									code: 'CHILD_PROCESS',
+								}),
+							)
+						}
+					})
+				}),
+			)
+		}
+	})
+
+	try {
+		await Promise.all(goals)
+	} catch (error) {
+		if (error.code !== 'CHILD_PROCESS') {
+			throw error
+		}
+
+		console.error(`\nSome benchmarks failed.`)
+		process.exit(1)
+	}
+}
 
 /**
  * List of the possible command-line flags that can be used with `wiz bench`.
@@ -169,86 +250,4 @@ export const benchFlags = {
 		default: Infinity,
 		describe: 'Maximum number of iterations to allow for a benchmark',
 	},
-}
-
-export async function benchCommand(argv) {
-	const runProfiler = Boolean(argv.profile)
-	const runSerially = Boolean(argv.serial || runProfiler)
-
-	process.env.WIZ_BENCH = JSON.stringify({
-		growthFn: argv.growth,
-		maxRunTime: argv.benchTime,
-		maxIterations: argv.benchRuns,
-	})
-
-	let targetShard = 0
-	let numBenchFiles = 0
-	const numCPUs = os.cpus().length
-	const fileShards = runSerially ? [[]] : [...new Array(numCPUs)].map(() => [])
-
-	for await (const { file, type } of findSourceFiles({
-		directory: path.join(process.cwd(), 'src'),
-		cache: {},
-	})) {
-		if (type === 'benchmark') {
-			++numBenchFiles
-			fileShards[targetShard].push(file)
-
-			if (++targetShard === fileShards.length) {
-				targetShard = 0
-			}
-		}
-	}
-	debug(`Sharded ${numBenchFiles} benchmark files across ${numCPUs} processes`)
-
-	const goals = []
-	fileShards.forEach(shard => {
-		if (shard.length) {
-			goals.push(
-				new Promise((resolve, reject) => {
-					const args = ['-e', '"void 0"']
-
-					shard.forEach(file => {
-						args.unshift(file)
-						args.unshift('--require')
-					})
-
-					if (runProfiler) {
-						args.unshift(require.resolve('./register-profiler.dist.js'))
-						args.unshift('--require')
-					}
-
-					debug(`Spawning benchmark process: %O`, {
-						args,
-					})
-					const child = spawn(process.execPath, args, {
-						stdio: 'inherit',
-					})
-
-					child.on('close', code => {
-						if (code === 0) {
-							resolve()
-						} else {
-							reject(
-								Object.assign(new Error(), {
-									code: 'CHILD_PROCESS',
-								}),
-							)
-						}
-					})
-				}),
-			)
-		}
-	})
-
-	try {
-		await Promise.all(goals)
-	} catch (error) {
-		if (error.code !== 'CHILD_PROCESS') {
-			throw error
-		}
-
-		console.error(`\nSome benchmarks failed.`)
-		process.exit(1)
-	}
 }
