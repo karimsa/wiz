bc3acd227832a2a64cf25aded33d5e196943e2ea
Karim Alibhai <karim@alibhai.co>
2019-08-24 17:51:43 -0400

    Add: @babel/register to bench

--- src/commands/bench.js
+++ src/commands/bench.js
@@ -108,6 +108,9 @@ export async function benchCommand(argv) {
 					args.unshift('--require')
 				}
 
+				args.unshift(require.resolve('@babel/register'))
+				args.unshift('--require')
+
 				debug(`Spawning benchmark process: %O`, {
 					args,
 				})
