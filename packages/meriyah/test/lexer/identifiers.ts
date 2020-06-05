6333f5397b79883b3b4b6976df33c8a601d7437a
Karim Alibhai <karim@alibhai.co>
2020-06-05 08:15:46 +0000

    Fix: more type issues

--- packages/meriyah/test/lexer/identifiers.ts
+++ packages/meriyah/test/lexer/identifiers.ts
@@ -138,7 +138,7 @@ describe('Lexer - Identifiers', () => {
 
   for (const [ctx, token, op, value] of tokens) {
     it(`scans '${op}' at the end`, () => {
-      const state = create(op, '', undefined);
+      const state = create(op, '', undefined, () => {});
       const found = scanSingleToken(state, ctx, 0);
 
       t.deepEqual(
@@ -158,7 +158,7 @@ describe('Lexer - Identifiers', () => {
     });
 
     it(`scans '${op}' with more to go`, () => {
-      const state = create(`${op} `, '', undefined);
+      const state = create(`${op} `, '', undefined, () => {});
       const found = scanSingleToken(state, ctx, 0);
 
       t.deepEqual(
@@ -180,7 +180,7 @@ describe('Lexer - Identifiers', () => {
 
   function fail(name: string, source: string, context: Context) {
     it(name, () => {
-      const state = create(source, '', undefined);
+      const state = create(source, '', undefined, () => {});
       t.throws(() => scanSingleToken(state, context, 0));
     });
   }
