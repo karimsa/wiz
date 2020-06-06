4264d217e5918b6707f4f6694f1e6459e8bbaea1
Karim Alibhai <karim@alibhai.co>
2020-06-06 17:35:04 +0000

    Add: more assertions to test-lint

--- packages/wiz/src/commands/__tests__/test-lint.js
+++ packages/wiz/src/commands/__tests__/test-lint.js
@@ -3,14 +3,16 @@ import { linter, createVirtualNode } from '../../__tests__/helpers'
 
 describe('Linter', () => {
 	it('should format files automatically', async () => {
-		const { text } = await lintFileNode({
+		const { text, summary, dirty } = await lintFileNode({
 			linter,
 			node: createVirtualNode({
-				text: `function f(a){let b=a;return b}`,
+				text: `export function f(a){let b=a;return b}`,
 			}),
 		})
+		expect(summary.messages).toHaveLength(0)
+		expect(dirty).toBe(true)
 		expect(text).toEqual([
-			'function f(a) {',
+			'export function f(a) {',
 			'	let b = a',
 			'	return b',
 			'}',
